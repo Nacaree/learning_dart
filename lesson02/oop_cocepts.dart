fa01ef354e78dc782c69ccc4169cd3d4c350724a
La <nacare13g@gmail.com>
2025-07-02 19:42:37 +0700

    modified oop_concepts

--- lesson02/oop_cocepts.dart
+++ lesson02/oop_cocepts.dart
@@ -17,6 +17,8 @@ class _Subject {
 
 void main() {
   Student stu1 = new Student("sothVI", 12);
+  // ignore: unused_local_variable
+  _Subject sub1 = new _Subject("Flutter", 9.3);
   // stu1.name = "sothVi";
   // stu1.age = 12;
   stu1.displayStuInfo();
