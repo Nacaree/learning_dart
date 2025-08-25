2a52f337afffea25f580197366e8ee30988b66d7
Nacare <nacare13g@gmail.com>
2025-08-26 00:06:13 +0700

    added stuff for function crash course

--- DartCrashCourse/functions.dart
+++ DartCrashCourse/functions.dart
@@ -1,9 +1,10 @@
 void main() {
-  String hello = greet("kan", 12);
+  final hello = greet(name: "kan", age: 12);
+  final hello2 = greet(age: 30, name: "SothVi");
   print(hello);
+  print(hello2);
 }
 
-
-String greet(name, age) {
+greet({String? name, required int age}) {
   return "My name is, $name, I am $age";
 }
